5b3b1fa8ecfb07cf83c9fed0230088880dd3c5a9
JoaoFreire1 <92170119+JoaoFreire1@users.noreply.github.com>
2025-05-09 12:30:21 +0100

    FilePower2

---
+++ FilePower2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FilePower2
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Console.WriteLine("filename: "+args[0]);
+            
+            Queue<string> qs = new Queue<string>(); 
+            string inp;
+
+            do
+            {
+                inp= Console.ReadLine();
+
+                if (inp=="")
+                    break;
+
+                qs.Enqueue(inp);
+
+            } while (true);
+
+
+
+            WriteAllLines(args[0], qs);
+
+
+
+
+
+
+            void WriteAllLines(string path, IEnumerable<string> contents)
+            {
+                using StreamWriter sr = new StreamWriter(path);
+
+                foreach (string s in contents)
+                {
+                    sr.WriteLine(s);
+                }
+
+                //sr.Close();
+            }
+            
+        }
+    }
+}
